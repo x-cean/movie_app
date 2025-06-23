8b7f71eceba7e3a9e9e346b17fe45968a88146ae
xiao <194778728+x-cean@users.noreply.github.com>
2025-06-23 21:31:19 +0200

    db path tested, create tables if not found

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
+import os
+
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import inspect
 
 from datamanager.sqlite_data_manager import SQLiteDataManager
 from datamanager.sql_data_models import db, User, Movie
 
 
-DATABASE_URL = "sqlite:///data/data.sqlite"
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'data', 'data.sqlite')
 
 
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 db.init_app(app)
+
+# create tables if they do not exist
+with app.app_context():
+    inspector = inspect(db.engine)
+    if not inspector.get_table_names():
+        db.create_all()
