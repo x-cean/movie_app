1957db2d898dce049f13e7f9b9b57df0608de3a0
xiao <194778728+x-cean@users.noreply.github.com>
2025-06-23 21:12:39 +0200

    test run flask init

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 from datamanager.sqlite_data_manager import SQLiteDataManager
+from datamanager.sql_data_models import db, User, Movie
 
 
-db = SQLiteDataManager(SQLiteDataManager("sqlite:///data/data.sqlite"))
+DATABASE_URL = "sqlite:///data/data.sqlite"
 
 
-print(db)
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
+db.init_app(app)
--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -6,5 +6,11 @@ class SQLiteDataManager(DataManagerInterface):
     def __init__(self, db_file_name):
         self.db = SQLAlchemy(db_file_name)
 
-    def get_user(self, movie_id):
-        pass
+    def get_all_users(self):
+        print("1")
+
+    def get_user_movies(self, user_id):
+        print("2")
+
+    def get_movie(self, movie_id):
+        print("3")
